78db2b3c8efd803366e6bc0a586bf03a3187854d
tomekbil <tomasz.bilaszewski@gmail.com>
2019-03-10 21:40:22 +0100

    splitted implementation to different files

--- cpp/dijkstra algorithm/algorithm.cc
+++ cpp/dijkstra algorithm/algorithm.cc
@@ -1,74 +1,7 @@
-#include <stdio.h>
-#include <vector>
-#include <list>
-#include <map>
-#include <string>
-#include <iostream>
-#include <climits>
-#include <type_traits>
+#include "logbuilder.cc"
 
-
-//#define MAX INT_MAX
-#define MAX 9999999
 #define DEBUG_LOG 0
 typedef unsigned uint;
-
-
-class LogBuilder {
-
-private:
-  std::list<std::string> list{};
-  std::string delimitter;
-  std::string message;
-  bool verbose;
-public:
-
-  LogBuilder(std::string _msg = "debug", std::string _del = "", bool _verbose = true ) :
-    delimitter(_del),
-    message(_msg),
-    verbose(_verbose)
-  {}
-
-  LogBuilder( std::initializer_list<std::string> _list, std::string _msg = "debug", std::string _del = " ", bool _verbose = true ) :
-    delimitter(_del),
-    message(_msg),
-    list(_list),
-    verbose(_verbose)
-  {}
-
-  template<typename T>
-  LogBuilder& add( T msg ) {
-    if( msg >= MAX )
-      list.push_back("MAX");
-    else
-      list.push_back( std::to_string(msg) );
-    return *this;
-  }
-
-  LogBuilder& add( std::string msg ) {
-    list.push_back( msg );
-    return *this;
-  }
-  LogBuilder& add( const char* msg ) {
-    list.push_back( std::string(msg) );
-    return *this;
-  }
-
-  void print() {
-    list.push_front(message);
-
-    auto it = list.begin();
-    auto last = --list.end();
-
-    if(verbose) {
-      for( ; it!=last; it++ ) {
-        std::cout << *it << delimitter;
-      }
-      std::cout << *last << std::endl;
-    }
-    list.clear();
-  }
-};
 
 #if DEBUG_LOG
 LogBuilder lb("debug log->", " ", true);
@@ -77,7 +10,6 @@ LogBuilder li("debug log->", " ", true);
 LogBuilder lb("debug log->", " ", false);
 LogBuilder li("debug log->", " ", false);
 #endif
-
 
 class Connection {
 
@@ -394,60 +326,3 @@ public:
   }
 
 };
-
-
-
-int main()
-{
-
-  Graph graph1("random");
-  // uint v1, v2, v3;
-  // for(uint i=0; i<100; i++) {
-  //      v1 = rand() % 11;
-  //      v2 = rand() % 11;
-  //      v3 = rand() % 101;
-  //      graph1.addConnection( new Connection(v1,v2,v3) );
-  // }
-  uint con, v, r;
-  std::cout << "Enter connections number: ";
-  std::cin >> con;
-  std::cout << "Enter vertex number: ";
-  std::cin>> v;
-  std::cout << "Enter range of cost: ";
-  std::cin >> r;
-
-  graph1.getRandom(con, v, r);
-  graph1.printConnections();
-  DijkstraAlgorithm algorithm(graph1);
-  algorithm.configureStart().run().showCostArray();
-
-  algorithm.configureEnd().showShortestPath().showCost();
-
-  // Graph graph1("grap1");
-  // graph1.addConnection( new Connection(0,1,3))
-  //       .addConnection( new Connection(0,4,3))
-  //       //.addConnection( Connection(1,0,3))
-  //       .addConnection( new Connection(1,2,1))
-  //       //.addConnection( Connection(2,1,1))
-  //       //.addConnection( Connection(2,4,3))
-  //       //.addConnection( Connection(3,2,3))
-  //       //.addConnection( Connection(3,5,1))
-  //       //.addConnection( Connection(4,0,3))
-  //       //.addConnection( Connection(5,2,1))
-  //       .addConnection( new Connection(4,5,2))
-  //       .addConnection( new Connection(5,3,1))
-  //       //.addConnection( Connection(5,4,2))//;
-  //       .addConnection( new Connection(5,0,6))
-  //       .addConnection( new Connection(3,1,3))
-  //       .addConnection( new Connection(2,5,1))
-  //       .addConnection( new Connection(2,3,1))
-  //       .addConnection( new Connection(1,2,1));
-
-  // graph1.printConnections();
-  // DijkstraAlgorithm algorithm(graph1);
-  // algorithm.configureStart().configureEnd().run().showShortestPath().showCost();
-
-
-
-
-}
--- cpp/dijkstra algorithm/dijkstra.cc
+++ cpp/dijkstra algorithm/dijkstra.cc
@@ -1,132 +1,61 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
 #include <vector>
 #include <list>
+#include <map>
+#include <string>
 #include <iostream>
+#include <climits>
+#include <type_traits>
 
-
-#define MAX 99999
-
-int findMIN(std::list<int> Q, std::vector<int> d) {
-    int min_vec;
-    int min_cost = MAX;
-
-    for( auto index: Q) {
-        if( d.at(index) < min_cost ) {
-            min_vec = index;
-            min_cost = d.at(index);
-        }
-    }
-
-    std::cout << "found min cost " << min_cost << " at " << min_vec << std::endl;
-    return min_vec;
-}
-
-bool contains(std::list<int> Q, int index) {
-
-    for( auto el: Q) {
-        if( el == index ) {
-            std ::cout << "Q contains " << index << std::endl;
-            return true;
-
-        }
-    }
-
-    return false;
-}
-
-bool hasConnection(std::vector<std::vector<int>> vec, int index1, int index2) {
-
-    if( vec[index1][index2] != -1) {
-        std ::cout << "connection between  " << index1 << " and " << index2 << std::endl;
-        return true;
-    }
-    else
-        return false;
-
-}
-
-std::vector<int> findPath(std::vector<int> p, int destination) {
-
-    std::vector<int> path{destination};
-    int vertex=destination;
-    while( vertex != 0 ) {
-        vertex = p.at(vertex);
-        path.push_back(vertex);
-    }
-
-    return path;
-}
-
+#include "algorithm.cc"
 
 int main()
 {
-    std::vector<std::vector<int>> vec{
-        {0, 3, -1, -1, 3,-1  },
-        {3, 0, 1, -1, -1, -1 },
-        {-1, 1, 0, 3, -1, 1  },
-        {-1, -1, 3, 0, -1, 1},
-        {3, -1, -1, -1, 0, 2 },
-        {-1, -1, 1, 1, 2, 0  }
-    };
 
-    std::list<int> Q{ 0, 1, 2, 3, 4, 5};
-    std::list<int> S{};
+  Graph graph1("random");
+  // uint v1, v2, v3;
+  // for(uint i=0; i<100; i++) {
+  //      v1 = rand() % 11;
+  //      v2 = rand() % 11;
+  //      v3 = rand() % 101;
+  //      graph1.addConnection( new Connection(v1,v2,v3) );
+  // }
+  uint con, v, r;
+  std::cout << "Enter connections number: ";
+  std::cin >> con;
+  std::cout << "Enter vertex number: ";
+  std::cin>> v;
+  std::cout << "Enter range of cost: ";
+  std::cin >> r;
 
-    std::vector<int> d { 0, MAX, MAX, MAX, MAX, MAX};
-    std::vector<int> p { -1, -1, -1, -1, -1, -1 };
+  graph1.getRandom(con, v, r);
+  graph1.printConnections();
+  DijkstraAlgorithm algorithm(graph1);
+  algorithm.configureStart().run().showCostArray();
 
-    while( Q.size() ) {
+  algorithm.configureEnd().showShortestPath().showCost();
 
-        int minVertexIndex = findMIN( Q, d);
-        S.push_back(minVertexIndex);
-        Q.remove(minVertexIndex);
+  // Graph graph1("grap1");
+  // graph1.addConnection( new Connection(0,1,3))
+  //       .addConnection( new Connection(0,4,3))
+  //       //.addConnection( Connection(1,0,3))
+  //       .addConnection( new Connection(1,2,1))
+  //       //.addConnection( Connection(2,1,1))
+  //       //.addConnection( Connection(2,4,3))
+  //       //.addConnection( Connection(3,2,3))
+  //       //.addConnection( Connection(3,5,1))
+  //       //.addConnection( Connection(4,0,3))
+  //       //.addConnection( Connection(5,2,1))
+  //       .addConnection( new Connection(4,5,2))
+  //       .addConnection( new Connection(5,3,1))
+  //       //.addConnection( Connection(5,4,2))//;
+  //       .addConnection( new Connection(5,0,6))
+  //       .addConnection( new Connection(3,1,3))
+  //       .addConnection( new Connection(2,5,1))
+  //       .addConnection( new Connection(2,3,1))
+  //       .addConnection( new Connection(1,2,1));
 
-        for( int i=0; i<6; i++) {
-            if( contains( Q, i) && hasConnection(vec, minVertexIndex, i)) {
-                int cost =  d[minVertexIndex] + vec[minVertexIndex][i];
-                if( d[i] > cost ) {
-                    std::cout << "d[i] > cost | " << d[i] << ">" << cost<< std::endl;
-                    d[i] = cost;
-                    p[i] = minVertexIndex;
-
-                }
-
-
-            }
-        }
-        std:: cout <<"d ";
-        for ( auto el: d) {
-        std:: cout << el << " ";
-        }
-        std::cout << std::endl;
-        std:: cout <<"p ";
-        for ( auto el: p) {
-            std:: cout << el << " ";
-        }
-        std::cout << std::endl;
-
-    }
-    int vertex;
-    std::cout << "Podaj wierzcholek do ktorego chcesz dojsc: "<< std::endl;
-    std::cin >> vertex;
-
-    std::vector<int> path = findPath(p, vertex);
-
-    std::cout << "Shortest path :";
-    for( auto el: path) {
-        std::cout << el << "->";
-    }
-    std::cout << std::endl;
-
-    std::cout << "Total Cost: " << d[vertex] << std::endl;
-
-    return 0;
+  // graph1.printConnections();
+  // DijkstraAlgorithm algorithm(graph1);
+  // algorithm.configureStart().configureEnd().run().showShortestPath().showCost();
 }
